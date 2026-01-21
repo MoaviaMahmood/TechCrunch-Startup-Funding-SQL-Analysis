4eba85b76f9573a13cabfa96f497fbd9ab0a97dd
Moavia Mahmood <168455506+MoaviaMahmood@users.noreply.github.com>
2026-01-21 17:02:03 +0300

    Delete Script/Intermediate Analysis/Locations with more than $1B total funding.sql

--- Script/Intermediate Analysis/Locations with more than $1B total funding.sql
+++
@@ -1,7 +0,0 @@
--- Locations with more than $1B total funding
-SELECT location,
-       SUM(raised) AS total_funding
-FROM TechCrunch_data
-GROUP BY location
-HAVING SUM(raised) > 1000000000
-ORDER BY total_funding DESC;
